225580eb67dd7f30ba6aca0f6254452b377060f1
Juan A Colindres <juan.colindres@delta-consulting.co>
2024-11-06 20:40:49 -0600

    Eslint & prettier setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-
 const Home = () => {
   return (
-    <h1 className="text-3xl">Welcome to the world of Nextjs</h1>
+    <h1 className="text-3xl font-black text-white">
+      Welcome to the world of Nextjs
+    </h1>
   );
-}
+};
 
-export default Home
+export default Home;
