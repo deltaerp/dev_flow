54cff26e27303d35dbb393c75cf0898156162222
Juan A Colindres <juan.colindres@delta-consulting.co>
2025-01-20 12:56:07 -0600

    Impement tag - questions action

--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -75,7 +75,7 @@ const TagCard = ({
 
   return (
     <Link href={ROUTES.TAG(_id)} className="shadow-light100_darknone">
-      <article className="background-light900_dark200 light-border flex w-full flex-col rounded-2xl border px-8 py-10 sm:w-[260px]">
+      <article className="background-light900_dark200 light-border flex w-full flex-row rounded-2xl border px-8 py-10 sm:w-[260px]">
         <div className="flex items-center justify-between gap-3">
           <div className="background-light800_dark400 w-fit rounded-sm px-5 py-1.5">
             <p className="paragraph-semibold text-dark300_light900">{name}</p>
